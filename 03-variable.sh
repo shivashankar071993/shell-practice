2488efba9c9bc6e48322c5cbe2bb938545a30d1e
shivashankar071993 <shivashankar071993@gmail.com>
2025-12-07 18:13:59 +0530

    some message

---
+++ 03-variable.sh
@@ -0,0 +1,9 @@
+#!bin/bash
+#No space before or after equl
+person1='john'
+person2='Raheem'
+
+echo "$person1:Hi $person2,How are you?"
+echo "$person2:Hello $person1, I am fine, How are you?"
+echo "$person1:what are you learning recently?"
+echo  "$person2:I am learning shell scripting $person1"
