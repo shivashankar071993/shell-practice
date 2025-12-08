bc3c166741cafc054f9e4e681d41ec0d9d32f341
shivashankar071993 <shivashankar071993@gmail.com>
2025-12-08 21:01:43 +0530

    Varaibles

--- 07-variable.sh
+++ 07-variable.sh
@@ -2,10 +2,10 @@
 
 #DATE=$(date)
 
-start_date=(date +%s)
-end_date=(date +%s)
+start_date=$(date +%s)
+end_date=$(date +%s)
 
 sleep 10 &
 
-total=$start_date-$end_date
+total=$($start_date-$end_date)
 echo "date is executed:" $total
