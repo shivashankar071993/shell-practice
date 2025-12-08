0dde33a1f209e4f180ba8cff3c5e8b0a48d94617
shivashankar071993 <shivashankar071993@gmail.com>
2025-12-08 22:05:02 +0530

    Varaibles

--- 08-special-variable.sh
+++ 08-special-variable.sh
@@ -3,7 +3,11 @@
 echo "ALL variables passed to the script: $@"
 echo "All variables passed to the script: $*"
 echo "The name of the script used :$0"
-echo "The current working directory of the sciprt:$pwd"
+echo "The current working directory of the sciprt:$PWD"
 echo "The home directory of the script used $HOME"
 echo "the user of the directory $USER"
 echo "The PID of the script is $$"
+
+sleep 50 &
+
+echo "The pid of the process running in background" $!
