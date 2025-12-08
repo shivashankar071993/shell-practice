421a31f349b1020bb1be9dbd6afaabc9ec4097af
shivashankar071993 <shivashankar071993@gmail.com>
2025-12-08 19:26:05 +0530

    Variables

--- 04-variable.sh
+++ 04-variable.sh
@@ -7,4 +7,4 @@ echo "$Person1:Welcome to alaska $Person2"
 echo "$Person2:Thankyou $Person1,Finally not in Zoom webinar"
 echo "$Person1:Do we have a deal"
 echo "$Person2:Yes, if you make Ukraine joins Russia"
- 
+  
