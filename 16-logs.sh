61f60841d71d75bc3fde86e4cf050f4863f43c02
shivashankar071993 <shivashankar071993@gmail.com>
2025-12-09 23:08:43 +0530

    conditions

---
+++ 16-logs.sh
@@ -0,0 +1,67 @@
+#!bin/bash
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+USERID=$(id -u)
+
+LOGS_FOLDER="/var/log/shell-script"
+SCRIPT_NAME=$(echo $0| cut -d "." -F1 )
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+
+mkdir -p $LOGS_FOLDER
+echo "SCRIPT started executing $(date)"
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ] ; then
+echo " Please run with root user else will not work"
+exit 1
+fi
+
+VALIDATE(){
+
+    if [ $1 -ne 0 ] ;  then 
+         echo -e "installation $2... $R failure $N"
+        exit 1
+    else
+         echo "instatllation $2... $G success $N"
+
+    fi
+}
+
+#CODE started 
+$(date) &>>$LOG_FILE
+##installing mysql
+dnf list installed mysql &>>$LOG_FILE
+# installed if it is not found
+
+if [ $? -ne 0 ]; then 
+dnf install mysql -y &>>$LOG_FILE
+VALIDATE $? "MYSQL"
+else 
+    echo -e "MYSQL Is already installed...$G Skipping $N"
+
+fi
+###installing nginx
+dnf list installed nginx &>>$LOG_FILE
+
+if [ $? -ne 0 ]; then 
+dnf install nginx -y &>>$LOG_FILE
+VALIDATE $? "nginx"
+else 
+    echo -e "NGINX Is already installed...$G Skipping $N"
+fi
+
+###installing python
+dnf list installed python3 &>>$LOG_FILE
+
+if [ $? -ne 0 ]; then 
+dnf install python3 -y  &>>$LOG_FILE
+VALIDATE $? "python"
+else 
+    echo -e "Python Is already installed...$G Skipping $N"
+fi
+#Coded ended
