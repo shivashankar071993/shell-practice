49209cbeecd8eb71be2c65dfe5ea9e85264e2163
shivashankar071993 <shivashankar071993@gmail.com>
2025-12-09 19:47:04 +0530

    conditions

--- 12-installation.sh
+++ 12-installation.sh
@@ -6,6 +6,8 @@ if [ $USER -ne 0 ] ; then
     echo "Please select the root user otherwise installation will not work"
 fi  
 
+exit 1 #failure code i
+
 dnf install mysqld -y
 
 if  [ $? -ne 0 ] ; then 
