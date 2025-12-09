fd7e8c36fedb854b3a984a36f7ac20be9118f150
shivashankar071993 <shivashankar071993@gmail.com>
2025-12-09 18:18:08 +0530

    conditions

--- 10-conditions.sh
+++ 10-conditions.sh
@@ -2,7 +2,7 @@
 
 NUMBER=$1
 
-if [ $NUMBER lt 10 ];
+if [ $NUMBER lt 10 ]; then 
     echo "The given number $NUMBER is smaller than 10"
 else 
     echo "The given number $NUMBER is greater than 10"
