95ea6d0ec10d147cc1614b591633479507f130d7
shivashankar071993 <shivashankar071993@gmail.com>
2025-12-10 17:49:16 +0530

    Loops

---
+++ 18-loops.sh
@@ -0,0 +1,44 @@
+ #!bin/bash
+
+#CODE started 
+
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+USERID=$(id -u)
+
+LOGS_FOLDER="/var/log/shell-script"
+SCRIPT_NAME=$(echo $0| cut -d "." -f1 )
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+
+mkdir -p $LOGS_FOLDER
+echo "SCRIPT started executing $(date)" | tee -a $LOG_FILE 
+
+USERID=$(id -u)
+
+date &>>$LOG_FILE
+
+if [ $USERID -ne 0 ] ; then
+echo " Please run with root user else will not work"
+exit 1
+fi
+
+VALIDATE(){
+
+    if [ $1 -ne 0 ] ;  then 
+         echo -e "installation $2... $R failure $N"
+        exit 1
+    else
+         echo "instatllation $2... $G success $N"
+
+    fi
+}
+
+
+for pacakge in $@
+do 
+    echo $pacakge
+done
