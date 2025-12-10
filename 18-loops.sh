5426ca5afbbd7ce60e8fc6642eeda7601b194729
shivashankar071993 <shivashankar071993@gmail.com>
2025-12-10 19:28:17 +0530

    Loops

--- 18-loops.sh
+++ 18-loops.sh
@@ -43,9 +43,9 @@ do
     #check whether package is installed or not
     dnf list installed $pacakge &>>LOG_FILE
     if [ $? -ne 0 ]; then 
-        dnf install $package -y &>>LOG_FILE
-        VALIDATE $? "$package"
+        dnf install $pacakge -y &>>LOG_FILE
+        VALIDATE $? "$pacakge"
     else 
-        echo -e "$package already installed...$Y SKipping $N"
+        echo -e "$pacakge already installed...$Y SKipping $N"
     fi
 done
