c61ea58eb61fa64d8b0950dfe7209e112699833c
shivashankar071993 <shivashankar071993@gmail.com>
2025-12-16 22:12:25 +0530

    sourcing

---
+++ 20-script-1.sh
@@ -0,0 +1,11 @@
+#!bin/bash
+
+NAME='INDIA'
+echo "MY country: $NAME"
+
+echo "PID of the SCRIPT1: $$"
+
+sh 21-script-2.sh
+
+
+
---
+++ 21-script-2.sh
@@ -0,0 +1,6 @@
+#!bin/bash
+
+NAME='INDIA'
+echo "MY country: $NAME"
+
+echo "PID of the SCRIPT2: $$"
