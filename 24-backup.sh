a83c2e04ae1c039707e02a56c706c90f9a999b63
shivashankar071993 <shivashankar071993@gmail.com>
2026-01-04 19:28:55 +0530

    shell-practice

---
+++ 24-backup.sh
@@ -0,0 +1,31 @@
+#!bin/bash
+
+#CODE started 
+
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+
+LOGS_FOLDER="/var/log/shell-script"
+SCRIPT_NAME=$(echo $0| cut -d "." -f1 )
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+
+
+mkdir -p $LOGS_FOLDER
+echo "SCRIPT started executing $(date)" | tee -a $LOG_FILE 
+
+if [ $USERID -ne 0 ] ; then
+echo " Please run with root user else will not work"
+exit 1
+fi
+
+USAGE() {
+
+    echo "USAGE:sudo sh  24-backup.sh <SOURCE_DIR> <DEST_DIR> <DAYS>[Optional ,default 14 days ]"
+}
+
+if [ $# lt 2 ]; then 
+    USAGE
