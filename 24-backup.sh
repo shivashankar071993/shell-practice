c4891419461bcfa1009684b04998f635db46fb2e
shivashankar071993 <shivashankar071993@gmail.com>
2026-01-04 20:34:17 +0530

    shell-practice

--- 24-backup.sh
+++ 24-backup.sh
@@ -57,7 +57,7 @@ if [ ! -z "${FILES}" ]; then
    TIMESTAMP=$(date +%F-%H-%M)
    ZIP_FILE_NAME="$DEST_DIR/app_logs_$TIMESTAMP.zip"
    echo "ZIP file name :$ZIP_FILE_NAME"
-   echo $FILES | zip -@ -j "$ZIP_FILE_NAME"
+   find $SOURCE_DIR -name "*.log" -type f -mtime +$Days | zip -@ -j "$ZIP_FILE_NAME"
 
 else 
     echo -e "No files to archive $Y Skipping $N"
