d62525c86adecc1fc732899af8441c558ad16107
shivashankar071993 <shivashankar071993@gmail.com>
2026-01-04 17:45:02 +0530

    shell-practice

---
+++ 22-delete-old-logs.sh
@@ -0,0 +1,31 @@
+#!bin/bash
+
+#CODE started 
+
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+
+LOGS_FOLDER="/var/log/shell-script"
+SCRIPT_NAME=$(echo $0| cut -d "." -f1 )
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+
+
+mkdir -p $LOGS_FOLDER
+echo "SCRIPT started executing $(date)" | tee -a $LOG_FILE 
+
+SOURCE_DIR=/home/ec2-user/app-logs
+
+if [ ! -d $SOURCE_DIR]; then
+
+echo  -e "ERROR:$SOURCE_DIR does not exits"
+exit 1
+fi
+
+
+
+FILES_TO_DELETE=$(find $SOURCE_DIR -name "*.log" -type f -mtime +7)
+
---
+++ 23-while_loop.sh
@@ -0,0 +1,16 @@
+#!bin/bash
+
+count=5
+
+echo "starting countdown"
+
+while [$count gt 5 ]
+
+do 
+    echo "time left" $count
+    sleep 1
+    count = $((count -1))
+
+done
+
+echo "times up"
